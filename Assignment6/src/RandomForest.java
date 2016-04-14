93433e685cff410d32ee5b369130ae2db8eb67c9
ThyRex3 <kathydai@email.uark.edu>
2016-04-14 00:16:52 -0500

    I'm tired. This is what I got.

--- Assignment6/src/RandomForest.java
+++ Assignment6/src/RandomForest.java
@@ -1,22 +1,23 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 class RandomForest extends SupervisedLearner
 {
 	double[] mode;
 	int numOfTrees;
 	ArrayList<DecisionTree> list;
+	Random rand;
 
 	RandomForest(){
 		this.numOfTrees = 0;
 		list = new ArrayList<DecisionTree>();
+		rand = new Random(1234);
 	}
 
 	RandomForest(int numOfTrees){
 		this.numOfTrees = numOfTrees;
 		list = new ArrayList<DecisionTree>();
-		for(int i = 0; i < this.numOfTrees){
-			list.add(new DecisionTree());
-		}
+		rand = new Random(1234);
 	}
 
 	String name()
@@ -24,20 +25,30 @@ class RandomForest extends SupervisedLearner
 		return "RandomForest";
 	}
 
-	// Should instantiate n DecisionTree instances
-	// Generate new training data for each DecisionTree by sampling with replacement. Then train.
 	// Give each DecisionTree an equal vote in predictions.
 	// For categorical labels, it predicts the value that is most common among the predictions of the models it aggregates. 
 	// For continuous labels, it predicts the mean of the predictions of the models it aggregates.
 	void train(Matrix features, Matrix labels)
 	{
-		mode = new double[labels.cols()];
-		for(int i = 0; i < labels.cols(); i++)
-		{
-			if(labels.valueCount(i) == 0)
-				mode[i] = labels.columnMean(i);
-			else
-				mode[i] = labels.mostCommonValue(i);
+		for(int i = 0; i < this.numOfTrees; i++){
+			DecisionTree temp = new DecisionTree();
+
+			Matrix thisFeatures = new Matrix();
+			thisFeatures.copyMetaData(features);
+			Matrix thisLabels = new Matrix();
+			thisLabels.copyMetaData(labels);
+
+			int randomRow = rand.nextInt(features.rows());
+
+			for(int j = 0; j < features.rows(); j++){
+				thisFeatures.newRow();
+				thisFeatures.copyBlock(thisFeatures.rows() - 1, 0, features, randomRow, 0, 1, features.cols());
+				thisLabels.newRow();
+				thisLabels.copyBlock(thisLabels.rows()-1, 0, labels, randomRow, 0, 1, labels.cols());
+			}
+
+			temp.train(thisFeatures, thisLabels);
+			list.add(temp);
 		}
 	}
 
