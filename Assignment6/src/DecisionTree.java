82660f0718041b532a9470ab13820f50a4eb8f83
ThyRex3 <kathydai@email.uark.edu>
2016-04-14 23:18:09 -0500

    Finished. Needs rewriting.

--- Assignment6/src/DecisionTree.java
+++ Assignment6/src/DecisionTree.java
@@ -65,11 +65,11 @@ class InteriorNode extends Node
 class LeafNode extends Node
 {
 	double[] label;
-
+	
 	LeafNode(){
-
+		
 	}
-
+	
 	LeafNode(Matrix labels){
 		label = new double[labels.cols()];
 		for(int i = 0; i < labels.cols(); i++)
@@ -89,23 +89,24 @@ class DecisionTree extends SupervisedLearner
 	Node root;
 	int minSize;
 	Random rand;
-
+	
 	DecisionTree(){
 		minSize = 5;
 		/// Seed of 1234, chaotic not random
 		rand = new Random(1456);
 	}
-
-	String name()
+	
+	String name() 
 	{
 		return "DecisionTree";
 	}
-
-	Node buildTree(Matrix features, Matrix labels){
+	
+	Node buildTree(Matrix features, Matrix labels)
+	{
 		// Chop data until it gets small = leafNode
 		/// Base Case
 		if(features.rows() != labels.rows())
-			throw new IllegalArgumentException("Feature rows doesn't equal label rows.");
+			throw new IllegalArgumentException("what?");
 		if(features.rows() < minSize){
 			return new LeafNode(labels);
 		}
@@ -115,7 +116,7 @@ class DecisionTree extends SupervisedLearner
 			int dim = rand.nextInt(features.cols());
 			double[] row = features.row(rand.nextInt(features.rows()));
 			double val = row[dim];
-
+			
 			Matrix thisFeatures = new Matrix();
 			thisFeatures.copyMetaData(features);
 			Matrix thisLabels = new Matrix();
@@ -124,7 +125,7 @@ class DecisionTree extends SupervisedLearner
 			thatFeatures.copyMetaData(features);
 			Matrix thatLabels = new Matrix();
 			thatLabels.copyMetaData(labels);
-
+			
 			if(features.valueCount(dim) == 0){
 				// Dim is continous
 				// All points < val go in otherMatrix
@@ -134,64 +135,62 @@ class DecisionTree extends SupervisedLearner
 						thisFeatures.copyBlock(thisFeatures.rows() - 1, 0, features, i, 0, 1, features.cols());
 						thisLabels.newRow();
 						thisLabels.copyBlock(thisLabels.rows()-1, 0, labels, i, 0, 1, labels.cols());
-					}
-					else{
+					}else{
 						thatFeatures.newRow();
 						thatFeatures.copyBlock(thatFeatures.rows() - 1, 0, features, i, 0, 1, features.cols());
-						thatLabels.newRow();						
+						thatLabels.newRow();
 						thatLabels.copyBlock(thatLabels.rows() - 1, 0, labels, i, 0, 1, labels.cols());
 					}
 				}
-			}
-			else{
-				// Dim is categorical
-				// All points == val go in otherMatrix]
+			}else{
 				for(int i = 0; i < features.rows(); i++){
+					//categorical
 					if(features.row(i)[dim] == val){
 						thisFeatures.newRow();
 						thisFeatures.copyBlock(thisFeatures.rows() - 1, 0, features, i, 0, 1, features.cols());
-						thisLabels.newRow();						
+						thisLabels.newRow();
 						thisLabels.copyBlock(thisLabels.rows() - 1, 0, labels, i, 0, 1, labels.cols());
-					}
-					else{
-						thatFeatures.newRow();						
+					}else{
+						thatFeatures.newRow();
 						thatFeatures.copyBlock(thatFeatures.rows() - 1, 0, features, i, 0, 1, features.cols());
-						thatLabels.newRow();						
+						thatLabels.newRow();
 						thatLabels.copyBlock(thatLabels.rows() - 1, 0, labels, i, 0, 1, labels.cols());
 					}
 				}
-
 			}
+			
 			InteriorNode node = new InteriorNode(dim, val, features.valueCount(dim));
 			node.a = buildTree(thisFeatures, thisLabels);
 			node.b = buildTree(thatFeatures, thatLabels);
-			
+						
 			return node;
 		}
 	}
 
-	void train(Matrix features, Matrix labels)
-	{		
-		root = buildTree(features, labels);	
+	void train(Matrix features, Matrix labels) 
+	{
+		root = buildTree(features, labels);
 	}
 
-	void predict(double[] in, double[] out)
-	{
+	void predict(double[] in, double[] out) {
 		Node n = root;
-		while(!n.isLeaf()){
-			if(((InteriorNode)n).isCont == 0){
-				if(in[((InteriorNode)n).attribute] < ((InteriorNode)n).pivot)
+		
+		while(!n.isLeaf()){			
+			if(((InteriorNode) n).isCont == 0){
+				if(in[((InteriorNode)n).attribute] < ((InteriorNode)n).pivot){
 					n = ((InteriorNode)n).a;
-				else
+				}else{
 					n = ((InteriorNode)n).b;
-			}
-			else{
-				if(in[((InteriorNode)n).attribute] == ((InteriorNode)n).pivot)
+				}
+			}else{
+				if(in[((InteriorNode) n).attribute] == ((InteriorNode)n).pivot){
 					n = ((InteriorNode)n).a;
-				else
+				}else{
 					n = ((InteriorNode)n).b;
+				}
 			}
 		}
+		
 		Vec.copy(out, ((LeafNode)n).label);
 	}
 }
--- Assignment6/src/RandomForest.java
+++ Assignment6/src/RandomForest.java
@@ -1,166 +1,175 @@
-import java.util.ArrayList;
+import java.util.Random;
 
-class RandomForest extends SupervisedLearner
-{
-	Node[] root;
-	int numOfTrees;
-	Random rand;
-	Matrix[] train_feats;
-	Matrix[] train_labs;
-	private final int MIN_SIZE = 25;
-	private final int DEPTH_TIMEOUT = 50;
+public class RandomForest extends SupervisedLearner { 
+    private final int MIN_SIZE = 15;
 
-	RandomForest(){
-		this.numOfTrees = 0;
-	}
+    Node[] root;
+    int numOfTrees;
+    Random rand;
+    Matrix[] train_feats;
+    Matrix[] train_labs;
+    
+    public RandomForest(int numOfTrees) {
+        this.numOfTrees = numOfTrees;
+        this.root = new Node[numOfTrees];
+        this.rand = new Random(1456);
+        
+        this.train_feats = new Matrix[numOfTrees];
+        this.train_labs = new Matrix[numOfTrees];
+        
+        
+        for (int i = 0; i < numOfTrees; i++) {
+            this.train_feats[i] = new Matrix();
+            this.train_labs[i] = new Matrix();
+        }
+    }
+    
+    @Override
+    String name() 
+    {
+        return "RandomForest";
+    }
+    
+    private Node buildTree(Matrix features, Matrix labels){
+        if(features.rows() != labels.rows())
+            throw new IllegalArgumentException("Feature rows doesn't equal label rows.");       
+        if(features.rows() < MIN_SIZE){
+            LeafNode nLeaf = new LeafNode();
+            double[] temp = new double[1];
+            temp[0] = labels.mostCommonValue(0);
+            nLeaf.label = temp;
+            return nLeaf; 
+        }else{
+            int dim = rand.nextInt(features.cols());
+            double[] row = features.row(rand.nextInt(features.rows()));
+            double val;
+            
+            Matrix thisFeatures = new Matrix();
+            thisFeatures.copyMetaData(features);
+            Matrix thisLabels = new Matrix();
+            thisLabels.copyMetaData(labels);
+            Matrix thatFeatures = new Matrix();
+            thatFeatures.copyMetaData(features);
+            Matrix thatLabels = new Matrix();
+            thatLabels.copyMetaData(labels);
 
-	RandomForest(int numOfTrees){
-		this.root = new Node[numOfTrees];
-		this.numOfTrees = numOfTrees;
-		this.rand = new Random(1234);
-		this.train_feats = new Matrix[numOfTrees];
-		this.train_labs = new Matrix[numOfTrees];
-
-		for(int i = 0; i < numOfTrees; i++){
-			this.train_feats[i] = new Matrix();
-			this.train_labs[i] = new Matrix();
-		}
-	}
-
-	@Override
-	String name()
-	{
-		return "RandomForest";
-	}
-
-	Node buildTree(Matrix features, Matrix labels, int depth){
-		if(features.rows() != labels.rows())
-			throw new IllegalArgumentException("Feature rows doesn't equal label rows.");
-		if(features.rows() < MIN_SIZE || depth > DEPTH_TIMEOUT){
-			// Return the most common value in column 0 of labels
-			LeafNode newLeaf = new LeafNode();
-			double[] temp = new double[1];
-			temp[0] = labels.mostCommonValue(0);
-			newLeaf.label = temp;
-			return newLeaf;
-		}
-		else {
-			int dim = rand.nextInt(features.cols());
-			double[] row = features.row(rand.nextInt(features.rows()));
-			double val = row[dim];
-			double value;
-
-			Matrix thisFeatures = new Matrix();
-			thisFeatures.copyMetaData(features);
-			Matrix thisLabels = new Matrix();
-			thisLabels.copyMetaData(labels);
-			Matrix thatFeatures = new Matrix();
-			thatFeatures.copyMetaData(features);
-			Matrix thatLabels = new Matrix();
-			thatLabels.copyMetaData(labels);
-
-            if (features.valueCount(dim) == 0) {
-                value = features.columnMean(dim);
-                for (int i = 0; i < features.rows(); i++) {     
-                	double[] fRow = features.row(i);
-					double[] lRow = labels.row(i);        
-                    if (val < value) {
+            if(features.valueCount(dim) == 0){
+                val = features.columnMean(dim);
+                
+                for(int i = 0; i < features.rows(); i++){
+                    double[] fRow = features.row(i);
+                    double[] lRow = labels.row(i);
+                    double value = fRow[dim];
+                    
+                    if (value < val){
                         double[] newFeatures = thisFeatures.newRow();
-						double[] newLabels = thisLabels.newRow();
-						for (int j = 0; j < features.cols(); j++) {
-							newFeatures[j] = fRow[j];
-						}
-						newLabels[0] = lRow[0];
-                    } else {
+                        double[] newLabels = thisLabels.newRow();
+                        
+                        for(int j = 0; j < features.cols(); j++)
+                            newFeatures[j] = fRow[j];
+                        
+                        newLabels[0] = lRow[0];
+                    }
+                    else{
                         double[] newFeatures = thatFeatures.newRow();
-						double[] newLabels = thatLabels.newRow();
-						for (int j = 0; j < features.cols(); j++) {
-							newFeatures[j] = fRow[j];
-						}
-						newLabels[0] = lRow[0];
+                        double[] newLabels = thatLabels.newRow();
+                        
+                        for(int j = 0; j < features.cols(); j++)
+                            newFeatures[j] = fRow[j];
+                        
+                        newLabels[0] = lRow[0];
                     }
                 }
-            } 
-            else {             
-                value = row[rand.nextInt(features.cols())];
-                for (int i = 0; i < features.rows(); i++) {    
-                	double[] fRow = features.row(i);
-					double[] lRow = labels.row(i);       
-                    if (value == val) {
+            }else{
+                val = row[rand.nextInt(features.cols())];
+                
+                for(int i = 0; i < features.rows(); i++){
+                    double[] fRow = features.row(i);
+                    double[] lRow = labels.row(i);
+                    double value = fRow[dim];
+                    
+                    if(value == val){
                         double[] newFeatures = thisFeatures.newRow();
-						double[] newLabels = thisLabels.newRow();
-						for (int j = 0; j < features.cols(); j++) {
-							newFeatures[j] = fRow[j];
-						}
-						newLabels[0] = lRow[0];
-                    } else {
+                        double[] newLabels = thisLabels.newRow();
+                        
+                        for(int j = 0; j < features.cols(); j++)
+                            newFeatures[j] = fRow[j];
+                        
+                        newLabels[0] = lRow[0];
+                    }else{
                         double[] newFeatures = thatFeatures.newRow();
-						double[] newLabels = thatLabels.newRow();
-						for (int j = 0; j < features.cols(); j++) {
-							newFeatures[j] = fRow[j];
-						}
-						newLabels[0] = lRow[0];
+                        double[] newLabels = thatLabels.newRow();
+                        
+                        for(int j = 0; j < features.cols(); j++)
+                            newFeatures[j] = fRow[j];
+                        
+                        newLabels[0] = lRow[0];
                     }
                 }
             }
-			InteriorNode node = new InteriorNode(dim, val, features.valueCount(dim));
-			node.a = buildTree(thisFeatures, thisLabels, depth++);
-			node.b = buildTree(thatFeatures, thatLabels, depth++);
+            
+            InteriorNode node = new InteriorNode(dim, val, features.valueCount(dim));
+            node.a = buildTree(thisFeatures, thisLabels);
+            node.b = buildTree(thatFeatures, thatLabels);
 
-			return node;
-		}
-
-	}
-
-	@Override	
-	void train(Matrix features, Matrix labels)
-	{
-		for (int i = 0; i < this.numOfTrees; i++) {
-			this.train_feats[i].copyMetaData(features);
-			this.train_labs[i].copyMetaData(labels);
-			
-			for (int j = 0; j < features.rows(); j++) {
-				int rowIndex = rand.nextInt(features.rows());
-				double[] fRow = features.row(rowIndex);
-				double[] lRow = labels.row(rowIndex);
-				
-				double[] tempRow = this.train_feats[i].newRow();
-				double[] tempLabel = this.train_labs[i].newRow();
-				
-				for (int k = 0; k < features.cols(); k++) {
-					tempRow[k] = fRow[k];
-				}
-				tempLabel[0] = lRow[0];
-			}
-			root[i] = buildTree(this.train_feats[i], this.train_labs[i], 0);
-		}
-	}
-
-	void predict(double[] in, double[] out)
-	{
-		Matrix poll = new Matrix();
-		poll.newColumn(100);
-
-		for(int i = 0; i < numOfTrees; i++){
-			Node n = root[i];
-
-			while(!n.isLeaf()){
-				if(((InteriorNode)n).isCont == 0){
-					if(in[((InteriorNode)n).attribute] < ((InteriorNode)n).pivot)
-						n = ((InteriorNode)n).a;
-					else
-						n = ((InteriorNode)n).b;
-				}
-				else{
-					if(in[((InteriorNode)n).attribute] == ((InteriorNode)n).pivot)
-						n = ((InteriorNode)n).a;
-					else
-						n = ((InteriorNode)n).b;
-				}
-			}
-			double[] temp = poll.newRow();
-			temp[0] = ((LeafNode)n).label[0];
-		}
-		out[0] = poll.mostCommonValue(0);
-	}
+            return node;
+        }
+    }
+    
+    @Override
+    void train(Matrix features, Matrix labels){
+        for(int i = 0; i < numOfTrees; i++){
+            this.train_feats[i].copyMetaData(features);
+            this.train_labs[i].copyMetaData(labels);
+            
+            for(int j = 0; j < features.rows(); j++){
+                int r = rand.nextInt(features.rows());
+                
+                double[] fRow = features.row(r);
+                double[] lRow = labels.row(r);
+                
+                double[] tempRow = this.train_feats[i].newRow();
+                double[] tempLabel = this.train_labs[i].newRow();
+                
+                for(int k = 0; k < features.cols(); k++)
+                    tempRow[k] = fRow[k];
+                
+                tempLabel[0] = lRow[0];
+            }
+            
+            root[i] = buildTree(this.train_feats[i], this.train_labs[i]);
+        }
+    }
+    
+    @Override
+    void predict(double[] in, double[] out){
+        Matrix poll  = new Matrix();
+        poll.newColumn();
+        
+        for(int i = 0; i < numOfTrees; i++){
+            Node n = root[i];
+            
+            while (!n.isLeaf()){
+                double val = in[((InteriorNode) n).attribute];
+                
+                if(((InteriorNode) n).isCont == 0){
+                    if(val < ((InteriorNode) n).pivot)
+                        n = ((InteriorNode)n).a;
+                    else
+                        n = ((InteriorNode)n).b;
+                }
+                else{
+                    if(val == ((InteriorNode) n).pivot)
+                        n = ((InteriorNode)n).a;
+                    else
+                        n = ((InteriorNode)n).b;
+                }
+            }
+            
+            double[] temp = poll.newRow();
+            temp[0] = ((LeafNode)n).label[0];
+        }
+        
+        out[0] = poll.mostCommonValue(0);
+    }
 }
